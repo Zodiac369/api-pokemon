ecc296a21bf2fdb3123cffbc1e38b16e53a25260
3MRE <emre.ogk@hotmail.fr>
2024-01-31 11:50:19 +0100

    🧑‍🚒

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -29,11 +29,7 @@ export class PokemonListComponent implements OnInit {
       getPokemonByID => {
         this.pokemonSelected = undefined;
         this.pokemonSelected = getPokemonByID;
-
-        console.log(this.pokemonSelected);
       });
   }
-
-
   
 }
