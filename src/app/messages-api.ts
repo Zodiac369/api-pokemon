9b1455301497ec4e374a1f4b34a18b13398dfaa2
3MRE <emre.ogk@hotmail.fr>
2024-01-31 18:43:25 +0100

    message  api interface

---
+++ src/app/messages-api.ts
@@ -0,0 +1,15 @@
+export interface MessagesApi {
+
+    true:string|undefined;
+    false:string|undefined;
+}
+
+/*
+    {
+        "true":"No have permission"
+    }
+    
+    {
+        "false":"Permission Denied"
+    }
+*/
